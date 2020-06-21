3beb53c914f8a05c1ebc737ec860abfd9855467c
Marcelo Escobar <escobar@br.systems>
2020-06-20 22:03:03 -0300

    react class

--- react_class/src/App.js
+++ react_class/src/App.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
-import ProjetoBase from './components/ProjetoBase/ProjetoBase';
+import { getNewTimeStamp} from './helpers/dateTimeHelpers';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      clickArray: []
+    };
+  }
+
+  handleClick = () => {
+    const newClickArray = Object.assign([],this.state.clickArray);
+    newClickArray.push(getNewTimeStamp());
+
+    this.setState({ clickArray: newClickArray });
+  }
+
+  componentDidUpdate() {
+    document.title = this.state.clickArray.length.toString();
+  }
+  
   render() {
-    return <ProjetoBase />;
+    const { clickArray } = this.state;
+    return (
+      <div>
+        <h1>React e <em>Class Components</em></h1>
+
+        <button onClick={this.handleClick}>Clique aqui</button>
+
+        <ul>
+          { clickArray.map((item) => {
+              return <li key={item}>{item}</li>;
+            })
+          };
+        </ul>
+      </div>
+    );
   }
 }
